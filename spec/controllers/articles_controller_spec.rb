842ed25b1765f3ca1404e5dc3ab88c8577aa061b
shibao <dream0184@yahoo.com.tw>
2021-10-08 16:43:35 +0800

    add new method in ArticlesController and rspec new method

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -37,10 +37,80 @@ RSpec.describe ArticlesController, type: :controller do
 
       expect(response).to render_template("show")
     end
+  end
 
+  describe "GET new" do 
+    it "assign @course" do
+      course = FactoryBot.build(:article)
 
+      get :new
 
+      expect(assigns(:article)).to be_a_new(Article)
+    end
+
+    it "render template" do 
+      article = FactoryBot.build(:article)
+
+      get :new
+
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do 
+    it "create a new article" do
+      article = FactoryBot.build(:article)
+
+      expect do 
+        post :create, params: { :article => FactoryBot.attributes_for(:article) }
+      end.to change{ Article.count }.by(1)
+    end
+
+    it "redirects to articles_path" do
+      article = FactoryBot.build(:article)
+
+      post :create, params: { :article => FactoryBot.attributes_for(:article) }
+
+      expect(response).to redirect_to articles_path
+    end
+
+    # 如果沒打標題，則不能儲存
+    it "doesn't create a record when article doesn't have a title" do 
+      expect do 
+        post :create, params: { article: { :body => "bar" } }
+      end.to change { Article.count }.by(0) 
+    end
+
+    it "render new template when article doesn't have title" do
+      post :create, params: { article: { :body => "bar" } }
+
+      expect(response).to render_template("new")
+    end
+
+    it "create a new article record when article has title" do
+      article = FactoryBot.build(:article)
+
+      expect do
+        post :create, params: { article: FactoryBot.attributes_for(:article) }
+      end.to change { Article.count }.by(1)
+    end
+
+    it "redirects to articles_path when article has title" do 
+      article = FactoryBot.build(:article)
+
+      post :create, params: { article: FactoryBot.attributes_for(:article) }
+
+      expect(response).to redirect_to articles_path
+    end
+
+    
 
 
   end
+
+
+
+
+
+
 end
